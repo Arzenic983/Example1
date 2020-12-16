16c79cf7bbb8599cd3bb3b87b682efeeffac39e6
Arseniy <ArsikXX@gmail.com>
2020-12-16 18:05:15 +1000

    main_2 - вторая задача

--- main_2.py
+++ main_2.py
@@ -1,6 +1,5 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
-from PyQt5.QtCore import QPointF
 from PyQt5.QtGui import QPainter, QColor
 import random
 
@@ -12,9 +11,9 @@ class Window(QMainWindow):
         self.flag = False
 
         self.pushButton = QPushButton('Нажми меня', self)
+        self.pushButton.clicked.connect(self.pressEvent)
         self.pushButton.resize(100, 60)
         self.pushButton.move(350, 500)
-        self.pushButton.clicked.connect(self.pressEvent)
 
     def pressEvent(self):
         self.flag = True
@@ -36,7 +35,6 @@ class Window(QMainWindow):
                 random.randint(10, 590),
                 size, size
             )
-
 
 
 if __name__ == '__main__':
